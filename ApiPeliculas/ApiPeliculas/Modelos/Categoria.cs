8abaf2613ae1fb86fb57bd41b42dd135cb067627
Alejandro Millán Jiménez <alejandro.millan@tvazteca.com.mx>
2024-12-19 11:19:43 -0600

    PROYECTO CURSO UDEMY

---
+++ ApiPeliculas/ApiPeliculas/Modelos/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPeliculas.Modelos
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }//Si pones el caracter '?' en el tipo de dato, se da chance de aceptar valores que no sean obligatorios
+        [Required]
+        [Display(Name = "Fecha de creación")] //Así se mostraría en una vista con el método 'Display', escribiéndolo así como está
+        public DateTime FechaCreacion { get; set; }
+    }
+}
