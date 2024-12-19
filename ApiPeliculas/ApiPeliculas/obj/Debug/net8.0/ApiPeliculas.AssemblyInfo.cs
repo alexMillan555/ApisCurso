9470a600b5222dc8dcc94dbfe8f3693df29fdecc
Alejandro Millán Jiménez <alejandro.millan@tvazteca.com.mx>
2024-12-19 14:01:42 -0600

    CAMBIOS NUEVOS

--- ApiPeliculas/ApiPeliculas/obj/Debug/net8.0/ApiPeliculas.AssemblyInfo.cs
+++ ApiPeliculas/ApiPeliculas/obj/Debug/net8.0/ApiPeliculas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiPeliculas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8abaf2613ae1fb86fb57bd41b42dd135cb067627")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiPeliculas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiPeliculas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
